eebf8fd709b6ae2fb2fcecdcf84d359378392b42
Roman Tsisyk <roman@tsisyk.com>
2019-12-25 16:50:38 +0300

    Remove Blockchain dependency from stegos_node::replication
    Needed for the light node.

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -71,7 +71,7 @@ use tokio_timer::{clock, Interval};
 const STAKE_FEE: i64 = 0;
 const RESEND_TX_INTERVAL: Duration = Duration::from_secs(2 * 60);
 const PENDING_UTXO_TIME: Duration = Duration::from_secs(5 * 60);
-const CHECK_PENDING_UTXO: Duration = Duration::from_secs(10);
+const CHECK_LOCKED_INPUTS: Duration = Duration::from_secs(10);
 
 ///
 /// Events.
@@ -162,7 +162,7 @@ struct UnsealedAccountService {
     resend_tx: Interval,
 
     /// Check for pending utxos.
-    check_pending_utxos: Interval,
+    expire_locked_inputs: Interval,
     //
     // Snowball state (owned)
     //
@@ -211,7 +211,7 @@ impl UnsealedAccountService {
         let epoch = database.epoch();
         debug!("Opened database: epoch={}", epoch);
         let resend_tx = Interval::new(clock::now(), RESEND_TX_INTERVAL);
-        let check_pending_utxos = Interval::new(clock::now(), CHECK_PENDING_UTXO);
+        let expire_locked_inputs = Interval::new(clock::now(), CHECK_LOCKED_INPUTS);
         let chain_notifications = ChainSubscription::new(&node, epoch, 0);
 
         info!("Loaded account {}", account_pkey);
@@ -224,7 +224,7 @@ impl UnsealedAccountService {
             network_pkey,
             database,
             resend_tx,
-            check_pending_utxos,
+            expire_locked_inputs,
             snowball,
             max_inputs_in_tx,
             network,
@@ -1015,7 +1015,7 @@ impl Future for UnsealedAccountService {
 
         loop {
             match self
-                .check_pending_utxos
+                .expire_locked_inputs
                 .poll()
                 .expect("no errors in timers")
             {
